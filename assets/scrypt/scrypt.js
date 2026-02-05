8f75678d6ef9ef4e1fb742f1e4bda9be6de0b9d1
unknown <gear.misha.mx@gmail.com>
2026-02-06 00:33:40 +0200

    feat:add adaptation for mobile phone; add adaptation header;add adaptation section find-car;add adaptation section search-car; add adaptaiton section how-work;add animation section brand-auto; add adaptation section choose-text; add adaptation section rent-car; add adaptation section reviews; add adaptation section app;add adaptation footer

---
+++ assets/scrypt/scrypt.js
@@ -0,0 +1,18 @@
+// const scrollAnimate = document.querySelector(".brand-auto")
+
+// scrollAnimate.innerHTML += scrollAnimate.innerHTML;
+// let pos = 0
+// let speed = 0.7
+
+// function animate() {
+//     pos -= speed
+
+
+//     if (Math.abs(pos) >= scrollAnimate.scrollWidth / 2) {
+//         pos = 0
+//     }
+
+//     scrollAnimate.style.transform = `translateX(${pos}px)`
+//     requestAnimationFrame(animate)
+// }
+// animate()
